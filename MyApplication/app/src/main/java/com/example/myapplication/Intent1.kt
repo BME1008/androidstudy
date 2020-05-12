0046aa6b4ba7f2c6585c661b92c56671b876e73b
백복만 <pd0a9813@posco.com>
2020-05-12 16:48:28 +0900

    s

--- MyApplication/app/src/main/java/com/example/myapplication/Intent1.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/Intent1.kt
@@ -21,21 +21,22 @@ class Intent1 : AppCompatActivity() {
         val c = ab+10
 
         change_activity.setOnClickListener {
-            // val intent  = Intent(this, Intent2::class.java) 이렇게 작성해도 됨
-/*          val intent  = Intent(this@Intent1, Intent2::class.java)     // Intent(보낼 정보, 어디로 갈껀지(이동 대상))
+        /*
+            val intent  = Intent(this, Intent2::class.java) 이렇게 작성해도 됨
+            val intent  = Intent(this@Intent1, Intent2::class.java)     // Intent(보낼 정보, 어디로 갈껀지(이동 대상))
 
             intent.putExtra("number1",1)
             intent.putExtra("number2",2)
 
             startActivity(intent)
-*/
-
+        */
             val intent2  = Intent(this@Intent1, Intent2::class.java)     // Intent(보낼 정보, 어디로 갈껀지(이동 대상))
 
             // .apply{}는 intent2 에 관련한 작업은 여기에 모두 있다고 명시하는거
             intent2.apply{
                 this.putExtra("number1",1)  // intent2.putExtra("number1",1) 와 같은거
                 this.putExtra("number2",1)  // intent2.putExtra("number2",1) 와 같은거
+                
                 // startActivity(intent2)   얘는 전달만 하는 역할
                 startActivityForResult(intent2,200)    // 얘는 전달을 하고 값까지 되돌려 받는 역할
             }
--- MyApplication/app/src/main/java/com/example/myapplication/Intent2.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/Intent2.kt
@@ -13,9 +13,7 @@ class Intent2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent2)
 
-        /*
-            get자료형Extra ex)getIntExtra, getStringExtra 처럼 사용, defaultValue 필수 null일 경우 대응
-        */
+        /* get자료형Extra ex)getIntExtra, getStringExtra 처럼 사용, defaultValue 필수 null일 경우 대응   */
         val number1 = intent.getIntExtra("number1",0)
         val number2 = intent.getIntExtra("numbe2r",0)
 
@@ -42,14 +40,15 @@ class Intent2 : AppCompatActivity() {
             setResult(Activity.RESULT_OK, resultIntent)   // setResult(-1) 로 작성해도 같음
             finish()    // Activity 종료
 
+        /*
+            Activity는 Stack으로 관리 됨 = 불러오는 순서대로 쌓임
+            Intent1 Activity에 Intent2 Activity 를 호출 화면 Activity1 > Activity2로 전환되고 Intent1 Activity 위에 Intent2 Activity 가 쌓이게됨
 
-            // Activity는 Stack으로 관리 됨 = 불러오는 순서대로 쌓임
-            // Intent1 Activity에 Intent2 Activity 를 호출 화면 Activity1 > Activity2로 전환되고 Intent1 Activity 위에 Intent2 Activity 가 쌓이게됨
-
-            // Intent2 Activity (2층)
-            // Intent1 Activity (1층)
-            //    ▼  Intent2 Activity를 finish() 종료하면
-            // Intent1 Activity (1층) 만 남게됨
+            Intent2 Activity (2층)
+            Intent1 Activity (1층)
+            ▼  Intent2 Activity를 finish() 종료하면
+            Intent1 Activity (1층) 만 남게됨
+        */
         }
 
     }
